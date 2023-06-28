f3ff2d508c157a7f00c32e688ef676b9324b3d39
ayushwashere <ayush.singh@appinventiv.com>
2023-06-29 01:44:57 +0530

    Q2,3,4 done

---
+++ 28JuneQ4.js
@@ -0,0 +1,26 @@
+console.log('-----while------');
+let i =0;
+while(i <10){
+    console.log(i);
+    i++;
+}
+console.log('----do while-------');
+let j =0;
+do{
+    console.log(j);
+    j++;
+}while(j <10){
+
+}
+console.log('----do while runs at least once-------');
+
+
+let k =10;
+do{
+    console.log(k);
+    k++;
+}
+while(k <10){
+
+}
+
