2c189c376685727f510e1f9938a3a383e5e13c21
ayushwashere <ayush.singh@appinventiv.com>
2023-07-07 15:23:37 +0530

    joi_validation

--- db/database.ts
+++ db/database.ts
@@ -8,10 +8,7 @@ export const connect = () => {
   // Connecting to the database
   mongoose
     .connect(MONGO_URI, {
-    //   useNewUrlParser: true,
-    //   useUnifiedTopology: true,
-    //   useCreateIndex: true,
-    //   useFindAndModify: false,
+      
     })
     .then(() => {
       console.log("Successfully connected to database");
